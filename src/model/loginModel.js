cb79a90802b9dd537c3bd730f9ec605d40847e02
Marie Marchal <you@example.com>
2025-04-08 15:16:36 +0200

    mise en place CI et test connexion

--- src/model/loginModel.js
+++ src/model/loginModel.js
@@ -1,23 +1,22 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({
     log: ["query"]
 });
 
-export const LoginModel = {
-    
+const LoginModel = {
     login: async (data) => {
         try {
             const user = await prisma.users.findFirst({
                 where: {
                     OR: [
-                        { email : data.email },
+                        { email: data.email },
                         { username: data.username }
                     ]
                 }
             });
-            
+
             if (!user) {
-                return { success: false, message: "User not found"};
+                return { success: false, message: "User not found" };
             }
 
             const isPasswordValid = data.password === user.password;
@@ -25,11 +24,14 @@ export const LoginModel = {
             if (!isPasswordValid) {
                 return { success: false, message: "Password is incorrect" };
             } else {
-                return { success: true, message: "Succesfully connected", userName: data.username };
+                return { success: true, message: "Successfully connected", userName: data.username };
             }
 
         } catch (error) {
-            console.error(error);            
+            console.error(error);
         }
     }
 };
+
+// Remplacer l'export `export const LoginModel` par `module.exports`
+module.exports = { LoginModel };  // On utilise `module.exports` pour exposer l'objet `LoginModel`
