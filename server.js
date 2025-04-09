11eb0d49b89a59742c1aecb5f69287b7d93c6ddf
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2025-04-09 09:52:10 +0200

    update

--- server.js
+++ server.js
@@ -9,7 +9,36 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+<<<<<<< HEAD
 app.use(login);
+=======
+
+console.log(prisma);
+
+//const newUser = await prisma.users.create({
+//    data: {
+//        username: 'gkrjgkjre',
+//        email: 'test@test.com',
+//        password: '123456',
+//        profile_picture: "test.com" 
+//    }
+//})
+
+//const { postInscription }  = require ("./src/model/inscription")
+app.post("/signup", async(req, res) => {
+    const { username, email, password } = req.body;
+    console.log(username)
+    const result = await prisma.users.create ({
+        data: {
+            username,
+            email,
+            password, 
+        }, 
+    })
+    res.json(result)
+})
+//app.post("/signup", postInscription)
+>>>>>>> f5b8142 (add post inscription)
 
 app.listen(process.env.PORT_SERVER, () => {
     console.log(`Server running on http://localhost:${process.env.PORT_SERVER}`);
