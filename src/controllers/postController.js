453804195854b5417dd11fcd8e3dd0fc3e03c626
Daniel <dvnielcueto@outlook.com>
2025-04-14 23:50:09 +0200

    import's renaming

---
+++ src/controllers/postController.js
@@ -0,0 +1,50 @@
+import { PostModel } from "../models/postModel.js";
+
+export const PostController =  {
+    getPostById: async (id) => {
+        try {
+            const result = await PostModel.findById(req.query.id);
+
+            if (!result) {
+                throw new Error("Post not found");
+            }
+            res.json(result);
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    getAllPosts : async (req, res) => {
+        try {
+            const result = await PostModel.findAll()
+            postView.displayMainPage(card(result))
+            return res.json(result)
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    createPost: async ( res, req) => {
+        const data = req.body;
+        const user_id = req.params;
+        try {
+            const result = await PostModel.addPost(data, user_id)
+            return res.json(result)
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    updatePost : async (req, res) => {
+        const data = req.body;
+        const id = req.params;
+        try {
+            const result = await PostModel.update(data, id)
+            return res.json(result)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+}
+
+
---
+++ src/models/postModel.js
@@ -0,0 +1,86 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ["query"]
+});
+
+export const PostModel = {
+
+    // findById: async (id) => {
+    //     try {
+    //         const result = await prisma.posts.findUnique({
+    //             where: {
+    //                 id: id
+    //             }
+    //         })
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // },
+
+    findAll: async () => {
+        try {
+            const result = await prisma.posts.findMany();
+            if (!result) {
+                throw new Error("Impossible de chercher posts");
+            }
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    // create: async (data, id) => {
+    //     try {
+    //         const result = await prisma.posts.create({
+    //             where: {
+    //                 user_id: id
+    //             },
+    //             data: {
+    //                 title: data.title,
+    //                 content: data.content
+    //             }
+    //         });
+            
+    //         if(!result){
+    //             throw new Error("impossible d'ajouter post");
+    //         }
+    //         return result;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // },
+
+    // update: async (data, id) => {
+    //     try {
+    //         const result = await prisma.posts.update({
+    //             where: {
+    //                 id : id
+    //             },
+    //             data:{
+    //                 title: title,
+    //                 content: content
+    //             }
+    //         })
+    //         if(!result){
+    //             throw new Error("impossible de modifier le post")
+    //         }
+    //         return result;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // },
+    
+    // delete: async (id) => {
+    //     try {
+    //         const result = await prisma.posts.delete({
+    //             where: {
+    //                 id: id
+    //             }
+    //         });
+    //         return result;
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // }
+}
