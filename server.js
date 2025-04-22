95d8a677bae42e18c0c6ef0b4760519f87d11aba
Daniel <dvnielcueto@outlook.com>
2025-04-22 10:51:39 +0200

    tests

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ import cors from 'cors';
 
 import { user } from './src/routes/userRoute.js';
 import { posts } from './src/routes/postRoute.js';
+import { auth } from './src/routes/authRoute.js';
 
 const app = express();
 dotenv.config();
 
-app.use(express.json());
-app.use(cors());
-app.use(user);
-app.use(posts);
-
-app.set('views', './src/views');
+app.set('views', './src/views/pages');
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index',  { title: 'Hey', message: 'Hello there!' });
-})
+app.use(express.json());
+app.use(cors());
+
+app.use(user);
+app.use(posts);
+app.use(auth);
 
 app.listen(process.env.PORT_SERVER, () => {
     console.log(`Server running on http://localhost:${process.env.PORT_SERVER}`);
