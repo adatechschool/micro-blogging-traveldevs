0afca96dd13885f62444530c3774d9daaacf25e7
Daniel <dvnielcueto@outlook.com>
2025-04-23 10:51:42 +0200

    lgin mvc implemented

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,20 @@
 import { AuthModel } from "../models/authModel.js";
-import { AuthView } from "../views/authView.js";
-import pug from 'pug';
-
 export const AuthController = {
     login: async (req, res) => {
         try {
-            res.render("login.pug");
             console.log(req.body);
             
             if (!AuthModel.login(req.body)) {
                 res.json({succes: false})
-                return new Error("unable to connect");
+                return new Error("Fatale error");
+            } else {
+                res.json({succes: true});
             }
-            //res.json({succes: true});
         } catch (error) {
             console.error(error);
         }
+    },
+    displayMainPage: async (req, res)=> {
+        res.render("login.pug");
     }
 }
