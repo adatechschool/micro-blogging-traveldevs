ad786114eb49aeeb2d534691c05f933afea873e6
GiauNGUYEN12 <giaunguyen0216@gmail.com>
2025-04-23 12:06:14 +0200

    Auth user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,21 @@
 import { AuthModel } from "../models/authModel.js";
+
 export const AuthController = {
     login: async (req, res) => {
         try {
-            console.log(req.body);
-            
+        console.log(req.body);
+        //const result = await AuthModel.login(req.body);
+
+        //if (!result) {
+        //    res.json({ success: true });
+        //} else {
+        //    res.json({ success: false});
+        //}
             if (!AuthModel.login(req.body)) {
                 res.json({succes: false})
                 return new Error("Fatale error");
-            } else {
+            } 
+             else {
                 res.json({succes: true});
             }
         } catch (error) {
--- src/models/authModel.js
+++ src/models/authModel.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import { PasswordUtils } from '../utils/passwordHasing.js';
 
 const prisma = new PrismaClient({
     log: ["query"]
