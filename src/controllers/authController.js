328e3a58a3837053cca349f0d69630d4e08e9b87
Daniel <dvnielcueto@outlook.com>
2025-04-23 13:18:18 +0200

    test2

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,21 +1,4 @@
 import { AuthModel } from "../models/authModel.js";
-<<<<<<< HEAD
-export const AuthController = {
-    login: async (req, res) => {
-        try {
-            console.log(req.body);
-            
-            if (!AuthModel.login(req.body)) {
-                res.json({succes: false})
-                return new Error("Fatale error");
-            } else {
-                res.json({succes: true});
-            }
-        } catch (error) {
-            console.error(error);
-        }
-    },
-=======
 
 export const AuthController = {
     login: async (req, res) => {
@@ -26,7 +9,6 @@ export const AuthController = {
         }            
     },
 
->>>>>>> main
     displayMainPage: async (req, res)=> {
         res.render("login.pug");
     }
